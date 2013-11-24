7748069a442f6bc17688ab20880eb2f2c7701b89
Eric Leclerc <eric.leclerc@gmail.com>
2013-11-24 13:58:30 -0500

    use Ansible inside the vm instead of on the host to make setup easier

--- provisioning/setup_ansible.sh
+++ provisioning/setup_ansible.sh
@@ -3,7 +3,11 @@
 ## Enable PPA and install Ansible
 
 # from http://www.ansibleworks.com/docs/intro_installation.html#latest-releases-via-apt-ubuntu
+
+apt-get update --yes
 apt-get install --yes python-software-properties
 add-apt-repository --yes ppa:rquillo/ansible
 apt-get update --yes
 apt-get install --yes ansible
+
+ansible-playbook -i /vagrant/provisioning/hosts /vagrant/provisioning/lamp.yml --connection=local
